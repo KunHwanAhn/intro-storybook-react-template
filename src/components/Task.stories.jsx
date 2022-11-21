9b579e6844ef9d90c68633cb4865b8be4fa28509
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-11-21 17:34:41 +0900

    storybook - addons

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -38,3 +38,12 @@ Archived.args = {
     state: 'TASK_ARCHIVED',
   },
 };
+
+export const LongTitle = Template.bind({});
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+LongTitle.args = {
+  task: {
+    ...DEFAULT_TASK,
+    title: longTitleString,
+  },
+};
